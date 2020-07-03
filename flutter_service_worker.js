9d8f5fc9e7d717cbc3612ae3fcc717234a9c936b
Berkay Dedeoglu <berkay.dedeoglu@renault.com>
2020-07-03 11:03:21 +0300

    init

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "0461e4f4ee29d79dad72b06781dc9c9d",
 "/": "0461e4f4ee29d79dad72b06781dc9c9d",
-"main.dart.js": "46b10390e92e8978ba3645781fb46d37",
+"main.dart.js": "1d381e054c961f213e5f54230e3ab855",
 "manifest.json": "5d1049409c00ffa0162bb99b845befbd"
 };
 
